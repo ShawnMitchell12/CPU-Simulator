5629cd42b851e6fbc6efa6fd2c6e7d481d055412
Shawn Mitchell <shawnmitch12@gmail.com>
2023-11-06 11:20:19 -0700

    Finished cpu class setup. Added operators, register and cache values

---
+++ CPU Simulator/cpu.py
@@ -0,0 +1,14 @@
+from cache import Cache
+from memory import Memory
+
+cpu_counter_init_value = 0
+number_of_registers = 9
+
+add_instruction_operator = 'ADD'
+add_i_instruction_operator = 'ADDI'
+jump_instruction_operator = 'J'
+cache_instruction_operator = 'cache'
+
+cache_off_value = 0
+cache_on_value = 1
+cache_flush_value = 2
